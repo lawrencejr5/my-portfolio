af1f8ae68fa56bdc5a1c60e967e9d3de54dd1b0b
lawrencejr5 <oputalawrence@gmail.com>
2025-05-12 19:40:49 +0100

    tech stack design done

--- app/page.tsx
+++ app/page.tsx
@@ -50,9 +50,9 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="about-container">
+      <div className="section-container about-container">
         <div className="header">
-          <h1>About_me</h1>
+          <h1>About_me:</h1>
           <button>collapse</button>
         </div>
         <div className="container">
@@ -77,6 +77,71 @@ export default function Home() {
                 <span>Astro_Physics</span>
                 <span>Human_Psychology</span>
               </p>
+            </div>
+          </div>
+          <p className="curly-braces close">{"}"}</p>
+        </div>
+      </div>
+
+      <div className="section-container skills-container">
+        <div className="header">
+          <h1>Tech_stack:</h1>
+          <button>collapse</button>
+        </div>
+        <div className="container">
+          <p className="curly-braces open">{"{"}</p>
+          <div className="stack-container">
+            <div className="content">
+              <strong>"Front_End_Dev":</strong>
+
+              <div className="sub-container">
+                <p className="sub-collapse">
+                  <span>-</span>&nbsp;collapse_items
+                </p>
+                <p className="square-braces open">{"["}</p>
+                <div className="sub-content">
+                  <div className="skill">React</div>
+                  <div className="skill">Typescript</div>
+                  <div className="skill">SCSS</div>
+                  <div className="skill">NextJs</div>
+                  <div className="skill">Jquery</div>
+                </div>
+                <p className="square-braces close">{"]"}</p>
+              </div>
+            </div>
+            <div className="content">
+              <strong>"Back_End_Dev":</strong>
+              <div className="sub-container">
+                <p className="sub-collapse">
+                  <span>-</span>&nbsp;collapse_items
+                </p>
+                <p className="square-braces open">{"["}</p>
+                <div className="sub-content">
+                  <div className="skill">Node.js</div>
+                  <div className="skill">Express.js</div>
+                  <div className="skill">MongoDb</div>
+                  <div className="skill">PHP</div>
+                  <div className="skill">MySql</div>
+                </div>
+                <p className="square-braces close">{"]"}</p>
+              </div>
+            </div>
+            <div className="content">
+              <strong>"Dev_Tools":</strong>
+              <div className="sub-container">
+                <p className="sub-collapse">
+                  <span>-</span>&nbsp;collapse_items
+                </p>
+                <p className="square-braces open">{"["}</p>
+                <div className="sub-content">
+                  <div className="skill">Git</div>
+                  <div className="skill">VsCode</div>
+                  <div className="skill">Canva</div>
+                  <div className="skill">Postman</div>
+                  <div className="skill">Cloudinary</div>
+                </div>
+                <p className="square-braces close">{"]"}</p>
+              </div>
             </div>
           </div>
           <p className="curly-braces close">{"}"}</p>
