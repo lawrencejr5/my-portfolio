1329b75ec9a5c1c8feb71c8be76228baf8297683
lawrencejr5 <oputalawrence@gmail.com>
2025-05-13 17:17:29 +0100

    nav on scroll up functions

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,32 +1,52 @@
 "use client";
 
-import React from "react";
-
+import React, { useEffect, useState, useRef } from "react";
 import Link from "next/link";
-
 import { MdOutlineFileDownload } from "react-icons/md";
 
 const Nav: React.FC = () => {
+  const [show, setShow] = useState(true);
+  const lastScrollY = useRef(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY < lastScrollY.current) {
+        // Scrolling up
+        setShow(true);
+      } else {
+        // Scrolling down
+        setShow(false);
+      }
+      lastScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="main-nav">
-      <div className="name">Oputa_Lawrence</div>
-      <div className="nav-links">
-        <Link className="link" href="/">
-          Home
-        </Link>
-        <Link className="link" href="#about">
-          About
-        </Link>
-        <Link className="link" href="#stack">
-          Skills
-        </Link>
-        <Link className="link" href="#projects">
-          Projects
-        </Link>
+    <nav className={`main-nav ${show ? "visible" : "hidden"}`}>
+      <div className="nav-container">
+        <div className="name">Oputa_Lawrence</div>
+        <div className="nav-links">
+          <Link className="link" href="/">
+            Home
+          </Link>
+          <Link className="link" href="#about">
+            About
+          </Link>
+          <Link className="link" href="#stack">
+            Skills
+          </Link>
+          <Link className="link" href="#projects">
+            Projects
+          </Link>
+        </div>
+        <button className="resume-btn">
+          Resume <MdOutlineFileDownload />
+        </button>
       </div>
-      <button className="resume-btn">
-        Resume <MdOutlineFileDownload />
-      </button>
     </nav>
   );
 };
